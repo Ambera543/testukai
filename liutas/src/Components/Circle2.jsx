3eadf1535f974362844025147bbc67c4b683ec91
Ambera543 <g.eismontiene@gmai.com>
2021-10-19 15:22:22 +0300

    klases darbas

---
+++ liutas/src/Components/Circle2.jsx
@@ -0,0 +1,14 @@
+
+function Circle2(props) {
+
+
+    return (
+        <>
+            <div className="circler" style={{backgroundColor:props.color}}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Circle2;
