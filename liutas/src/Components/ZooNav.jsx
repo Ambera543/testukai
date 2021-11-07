c4ce59b8ad5e139c30749e16b1a952a27dd6a199
Ambera543 <g.eismontiene@gmail.com>
2021-11-07 19:14:12 +0200

    veikia

--- liutas/src/Components/ZooNav.jsx
+++ liutas/src/Components/ZooNav.jsx
@@ -1,61 +1,62 @@
 import { useState } from "react";
 
-function ZooNav({ types, filter, reset, search, sort}) {
+function ZooNav({ types, filter, reset, search, sort }) {
+  const [filterValue, setFilterValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [sortValue, setSortValue] = useState("");
 
-    const [filterValue, setFilterValue] = useState('');
-    const [searchValue, setSearchValue] = useState('');
-    const [sortValue, setSortValue] = useState('');
+  const selectFilter = (e) => {
+    setFilterValue(e.target.value);
+    filter(e.target.value);
+  };
+  const selectSort = (e) => {
+    setSortValue(e.target.value);
+    sort(e.target.value);
+  };
+  const handleSearchValue = (e) => {
+    console.log(e);
+    setSearchValue(e.target.value);
+    search(e.target.value);
+  };
 
-    const selectFilter = e => {
-        setFilterValue(e.target.value);
-        filter(e.target.value)
-    }
-    const selectSort = e => {
-        setSortValue(e.target.value);
-        sort(e.target.value)
-    }
-    const handleSearchValue = e => {
-        console.log(e)
-        setSearchValue(e.target.value);
-        search(e.target.value)
-    }
+  const resetHandler = () => {
+    reset();
+    setFilterValue("");
+  };
 
-    const resetHandler = () => {
-        reset();
-        setFilterValue('');
-    }
+  return (
+    <div className="zoo_nav">
+      <div className="zoo_nav_filter">
+        <span>Animal Sort</span>
+        <select onChange={selectSort} value={sortValue}>
+          <option value="">Select animal</option>
+          <option value="name_asc">By name - AZ</option>
+          <option value="name_desc">By name - ZA</option>
+          <option value="weight_asc">By weight - 19</option>
+          <option value="weight_desc">By weight - 91</option>
+        </select>
+      </div>
 
-    return (
-        <div className="zoo_nav">
-             <div className="zoo_nav_filter">
-                <span>Animal Sort</span>
-                <select onChange={selectSort} value={sortValue}>
-                    <option value="">Select animal</option>
-                    <option value="name_asc">By name - AZ</option>
-                    <option value="name_desc">By name - ZA</option>
-                    <option value="weight_asc">By weight - 19</option>
-                    <option value="weight_desc">By weight - 91</option>
-                </select>
-            </div>
-        
-            <div className="zoo_nav_filter">
-                <span>Animal Filter by type</span>
-                <select onChange={selectFilter} value={filterValue}>
-                    <option value="">Select animal</option>
-                    {
-                        types.map(t => <option key={t.type} value={t.type}>{t.type}</option>)
-                    }
-                </select>
-            </div>
-            <div className="zoo_nav_filter">
-                <span>Animal Search by name</span>
-                <input onChange={handleSearchValue} value={searchValue}></input>
-            </div>
-            <div className="zoo_nav_reset">
-                <button onClick={resetHandler}>Reset</button>
-            </div>
-        </div>
-    )
+      <div className="zoo_nav_filter">
+        <span>Animal Filter by type</span>
+        <select onChange={selectFilter} value={filterValue}>
+          <option value="">Select animal</option>
+          {types.map((t) => (
+            <option key={t.type} value={t.type}>
+              {t.type}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="zoo_nav_filter">
+        <span>Animal Search by name</span>
+        <input onChange={handleSearchValue} value={searchValue}></input>
+      </div>
+      <div className="zoo_nav_reset">
+        <button onClick={resetHandler}>Reset</button>
+      </div>
+    </div>
+  );
 }
 
 export default ZooNav;
